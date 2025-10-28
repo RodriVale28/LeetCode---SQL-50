714158a000d6e3684d48c34b1d2bf76c6d8e494c
Valeria Rodriguez <179888914+RodriVale28@users.noreply.github.com>
2025-10-27 21:53:32 -0500

    Create SQL query for recyclable and low-fat products
    Add SQL query to select recyclable and low-fat products.

---
+++ EASY/Recyclable and Low Fat Products.sql
@@ -0,0 +1,6 @@
+-- Problem:
+-- URL:  https://leetcode.com/problems/recyclable-and-low-fat-products/submissions/?envType=study-plan-v2&envId=top-sql-50
+  
+select product_id --selecting variable
+from products 
+where low_fats = 'Y' and recyclable = 'Y' --using and to combine both conditions
